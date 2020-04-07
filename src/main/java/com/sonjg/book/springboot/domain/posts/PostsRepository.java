981da2e7c96af48435c0a42e3b26367cd3d9fb4d
jgson89 <jgson89@gogiya.kr>
2020-04-07 17:50:49 +0900

    116p ~update test

---
+++ src/main/java/com/sonjg/book/springboot/domain/posts/PostsRepository.java
@@ -0,0 +1,6 @@
+package com.sonjg.book.springboot.domain.posts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostsRepository extends JpaRepository<Posts, Long> {
+}
